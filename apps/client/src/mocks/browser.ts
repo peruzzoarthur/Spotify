ab26ba2f5857129aab42e02c90a1fd5a843c9b21
sp3ruzzo <peruzzoarthur@gmail.com>
2024-02-19 17:59:14 -0300

    carousel working on genres route

--- apps/client/src/mocks/browser.ts
+++ apps/client/src/mocks/browser.ts
@@ -1,22 +1,22 @@
 import { setupWorker } from "msw/browser";
 import { searchHandlers } from "./handlers/searchHandlers";
-// import { tracksHandlers } from "./handlers/tracksHandlers";
-// import { playlistHandlers } from "./handlers/playlistHandlers";
-// import { albumHandlers } from "./handlers/albumHandlers";
-// import { likeSongsHandlers } from "./handlers/likedSongsHandlers";
-// import { tokenHandler } from "./handlers/tokenHandler";
-// import { meHandlers } from "./handlers/meHandlers";
-// import { audioFeaturesHandlers } from "./handlers/audioFeaturesHandlers";
-// import { recHandlers } from "./handlers/recHandlers";
+import { tracksHandlers } from "./handlers/tracksHandlers";
+import { playlistHandlers } from "./handlers/playlistHandlers";
+import { albumHandlers } from "./handlers/albumHandlers";
+import { likeSongsHandlers } from "./handlers/likedSongsHandlers";
+import { tokenHandler } from "./handlers/tokenHandler";
+import { meHandlers } from "./handlers/meHandlers";
+import { audioFeaturesHandlers } from "./handlers/audioFeaturesHandlers";
+import { recHandlers } from "./handlers/recHandlers";
 
 export const worker = setupWorker(
-  ...searchHandlers
-  // ...playlistHandlers,
-  // ...tokenHandler,
-  // ...albumHandlers,
-  // ...likeSongsHandlers,
-  // ...meHandlers,
-  // ...tracksHandlers,
-  // ...audioFeaturesHandlers,
-  // ...recHandlers
+  ...searchHandlers,
+  ...playlistHandlers,
+  ...tokenHandler,
+  ...albumHandlers,
+  ...likeSongsHandlers,
+  ...meHandlers,
+  ...tracksHandlers,
+  ...audioFeaturesHandlers,
+  ...recHandlers
 );
